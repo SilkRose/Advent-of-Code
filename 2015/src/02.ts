7a13631eeae3c6f4ee051317f4c26cc0bf448254
Silk Rose <silkrose@love-tolerance.com>
2023-10-30 12:09:08 -0400

    Combine 2015 day 2 parts 1 and 2

--- 2015/src/02.ts
+++ 2015/src/02.ts
@@ -3,15 +3,18 @@ import { get_input } from "./lib.js";
 
 async function mane() {
 	const input = (await get_input(2015, 2)).trim().split("\n");
+	let area = 0;
 	let feet = 0;
 	for (let box of input) {
 		let [l, w, h] = box.split("x").map(Number);
+		let areas = [(l * w * 2), (w * h * 2), (h * l * 2)]
+		area += areas.reduce((a, b) => a + b) + (areas.sort((a, b) => a - b)[0] / 2);
 		let perimeter = ([l, w, h].sort((a, b) => a - b)[0] + [l, w, h].sort((a, b) => a - b)[1]) * 2
 		let cubic_area = (l * w * h)
 		feet += perimeter + cubic_area;
 	}
-
-	console.log(feet);
+	console.log("Part 1: " + area);
+	console.log("Part 2: " + feet);
 }
 
 mane();
