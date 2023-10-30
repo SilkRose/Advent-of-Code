e83f734df676120b0e04465e807c15f0bc33fffc
Silk Rose <silkrose@love-tolerance.com>
2023-10-30 12:06:11 -0400

    Combine 2015 day 1 parts 1 and 2

--- 2015/src/01.ts
+++ 2015/src/01.ts
@@ -5,17 +5,19 @@ import { exit } from "process";
 async function mane() {
 	const input = await get_input(2015, 1);
 	let level = 0;
+	let basement_enter: number;
 	[...input].forEach(function (char, i) {
 		if (char === "(") {
 			level += 1;
 		} else if (char === ")") {
 			level -= 1;
 		}
-		if (level === -1) {
-			console.log(i + 1);
-			exit(0)
+		if (!basement_enter && level === -1) {
+			basement_enter = i + 1;
 		}
   });
+  console.log("Part 1: " + level);
+  console.log("Part 2: " + basement_enter!);
 }
 
 mane();
