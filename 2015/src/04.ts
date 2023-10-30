08eb6155aef17f4538d13b3585c67bec7b2aec11
Silk Rose <silkrose@love-tolerance.com>
2023-10-30 13:03:10 -0400

    Combine 2015 day 4 parts 1 and 2

--- 2015/src/04.ts
+++ 2015/src/04.ts
@@ -1,15 +1,19 @@
 import "@total-typescript/ts-reset";
-import { Md5 } from 'ts-md5';
+import { Md5 } from "ts-md5";
 
 async function mane() {
 	const input = "ckczppom";
 	let i = 0;
 	let hash: string = "";
+	let part1: number;
 	while (hash.substring(0, 6) !== "000000") {
+		if (part1! === undefined && hash.startsWith("00000")) part1 = i - 1;
 		hash = Md5.hashStr(input + i);
 		i++;
 	}
-	console.log(i - 1);
+	let part2 = i - 1;
+	console.log("Part 1: " + part1!);
+	console.log("Part 2: " + part2);
 }
 
 mane();
