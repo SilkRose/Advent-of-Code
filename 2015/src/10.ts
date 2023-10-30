00b2a70588d5ea3976c86b7d2300ce20a4228db5
Silk Rose <silkrose@love-tolerance.com>
2023-10-30 18:59:43 -0400

    Combine 2015 day 10 parts 1 and 2

--- 2015/src/10.ts
+++ 2015/src/10.ts
@@ -3,13 +3,17 @@ import "@total-typescript/ts-reset";
 async function mane() {
 	const input = "1113122113";
 	let segments = [...input.match(/(\d)\1*/g)!];
+	let part_one;
 	for (let i = 0; i < 50; i++) {
 		segments = segments
 			.map((s) => s.length + s.slice(0, 1))
 			.join("")
 			.match(/(\d)\1*/g)!;
+		if (i === 39) part_one = segments.join("").length;
 	}
-	console.log(segments.join("").length);
+	const part_two = segments.join("").length;
+	console.log("Part 1: " + part_one);
+	console.log("Part 2: " + part_two);
 }
 
 mane();
