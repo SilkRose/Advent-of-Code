8665951d60dcef7546e2c7687c43261e2047dcc3
Silk Rose <silkrose@love-tolerance.com>
2023-10-30 20:16:55 -0400

    Combine 2015 day 12 parts 1 and 2

--- 2015/src/12.ts
+++ 2015/src/12.ts
@@ -4,23 +4,27 @@ import { get_input } from "./lib.js";
 async function mane() {
 	const input = await get_input(2015, 12);
 	const json = JSON.parse(input);
-	const sum = sum_without_red(json);
-	console.log(sum);
+	const part_one = sum_without_red(json);
+	const part_two = sum_without_red(json, true);
+	console.log("Part 1: " + part_one);
+	console.log("Part 2: " + part_two);
 }
 
-function sum_without_red(json: any): number {
+function sum_without_red(json: any, no_red?: boolean): number {
 	let total = 0;
 	obj: if (Object.keys(json).length > 0 && json.constructor === Object) {
-		for (let key in json) {
-			if (json[key] === "red") break obj;
+		if (no_red) {
+			for (let key in json) {
+				if (json[key] === "red") break obj;
+			}
 		}
 		for (let key in json) {
 			if (Number(json[key])) {
 				total += Number(json[key]);
 			} else if (typeof json[key] === "object") {
-				total += sum_without_red(json[key]);
+				total += sum_without_red(json[key], no_red);
 			} else if (Array.isArray(json[key])) {
-				total += sum_without_red(json[key]);
+				total += sum_without_red(json[key], no_red);
 			}
 		}
 	} else if (Array.isArray(json)) {
@@ -28,9 +32,9 @@ function sum_without_red(json: any): number {
 			if (Number(element)) {
 				total += Number(element);
 			} else if (typeof element === "object") {
-				total += sum_without_red(element);
+				total += sum_without_red(element, no_red);
 			} else if (Array.isArray(element)) {
-				total += sum_without_red(element);
+				total += sum_without_red(element, no_red);
 			}
 		}
 	}
