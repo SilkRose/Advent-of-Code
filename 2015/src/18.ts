ff81963bcb24f9e4ad5e46549a500b8f1eaa8231
Silk Rose <silkrose@love-tolerance.com>
2023-11-02 22:30:56 -0400

    Add 2015 day 18 part 2

--- 2015/src/18.ts
+++ 2015/src/18.ts
@@ -3,9 +3,12 @@ import { get_input } from "./lib.js";
 
 async function mane() {
 	const input = (await get_input(2015, 18)).trim().split("\n");
-	const map = get_map(input);
-	const part_one = run_sim(map);
+	const map1 = get_map(input);
+	const map2 = map1.map((m) => [...m]);
+	const part_one = run_sim(map1, false);
+	const part_two = run_sim(map2, true);
 	console.log("Part 1: " + part_one);
+	console.log("Part 2: " + part_two);
 }
 
 function get_map(input: string[]): boolean[][] {
@@ -22,15 +25,17 @@ function get_map(input: string[]): boolean[][] {
 	return map;
 }
 
-function run_sim(map: boolean[][]): number {
+function run_sim(map: boolean[][], stuck_corners: boolean): number {
 	for (let i = 0; i < 100; i++) {
-		const old_map = map.map(m => [...m]);
+		if (stuck_corners) map = stick_corners(map);
+		const old_map = map.map((m) => [...m]);
 		for (let x = 0; x < map.length; x++) {
 			for (let y = 0; y < map[x].length; y++) {
 				map[x][y] = check_light(old_map, old_map[x][y], x, y);
 			}
 		}
 	}
+	if (stuck_corners) map = stick_corners(map);
 	return map.flat(1).filter((l) => l).length;
 }
 
@@ -66,6 +71,14 @@ function check_light(
 	} else {
 		return alive === 3;
 	}
+}
+
+function stick_corners(map: boolean[][]): boolean[][] {
+	map[0][0] = true;
+	map[map.length - 1][0] = true;
+	map[0][map[0].length - 1] = true;
+	map[map.length - 1][map[0].length - 1] = true;
+	return map;
 }
 
 mane();
