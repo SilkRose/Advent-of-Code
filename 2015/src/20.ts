c1902e47b4967879a243d4ae4ddb76e827b710ca
Silk Rose <silkrose@love-tolerance.com>
2023-11-05 11:58:36 -0500

    Add 2015 day 20 part 2

--- 2015/src/20.ts
+++ 2015/src/20.ts
@@ -3,7 +3,9 @@ import "@total-typescript/ts-reset";
 async function mane() {
 	const input = 36000000;
 	const part_one = find_house(input);
+	const part_two = find_house_50(input);
 	console.log("Part 1: " + part_one);
+	console.log("Part 2: " + part_two);
 }
 
 function find_house(presents: number): number {
@@ -12,6 +14,20 @@ function find_house(presents: number): number {
 		let house = 0;
 		for (let p = 1; p <= i; p++) {
 			if (i % p === 0) house += p * 10;
+		}
+		if (presents <= house) return i;
+	}
+	return max;
+}
+
+function find_house_50(presents: number): number {
+	const max = presents / 11;
+	const floor = Math.floor(presents / 50);
+	for (let i = floor; i <= max; i++) {
+		let house = 0;
+		const min = Math.floor(i / 50);
+		for (let p = min; p <= i; p++) {
+			if (i % p === 0) house += p * 11;
 		}
 		if (presents <= house) return i;
 	}
